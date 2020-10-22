f072d07516985126a0a0af4ce9aa56621b1cf96e
Jake <jcmartin987@gmail.com>
2020-10-21 20:20:08 -0600

    lab9

--- lab_9/Lab_Website_3/server.js
+++ lab_9/Lab_Website_3/server.js
@@ -27,7 +27,7 @@ const dbConfig = {
 	port: 5432,
 	database: 'football_db',
 	user: 'postgres',
-	password: 'pwd'
+	password: 'qwerty'
 };
 
 var db = pgp(dbConfig);
@@ -97,11 +97,26 @@ app.get('/register', function(req, res) {
 
 /*Add your other get/post request handlers below here: */
 app.get('/home', function(req, res) {
-	res.render('pages/home',{
-		my_title:'Home Page',
-		color: 'FF0000',
-		color_msg: 'The Color Red'
-	});
+	var query = 'select * from favorite_colors;';
+	db.any(query)
+        .then(function (rows) {
+            res.render('pages/home',{
+				my_title: "Home Page",
+				data: rows,
+				color: '',
+				color_msg: ''
+			})
+
+        })
+        .catch(function (err) {
+            console.log('error', err);
+            response.render('pages/home', {
+                title: 'Home Page',
+                data: '',
+                color: '',
+                color_msg: ''
+            })
+        })
 });
 
 app.listen(3000);

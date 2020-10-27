37c443ac338415eb646a93cdfa70876d5fbf8c9a
Jake <jcmartin987@gmail.com>
2020-10-27 04:30:26 -0600

    lab9

--- lab_9/Lab_Website_3/server.js
+++ lab_9/Lab_Website_3/server.js
@@ -236,7 +236,7 @@ app.get('/player_info', function(req, res) {
     })
 });
 app.get('/player_info/post', function(req, res) {
-  player_id = req;
+  player_id = req.query.player_choice;
   console.log(player_id)
 	var query = "SELECT id, name FROM football_players;";
   var pinfo = "SELECT * FROM football_players WHERE id = " + player_id + ";";

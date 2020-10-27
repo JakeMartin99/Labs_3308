409c8d6f1651f93b24d2164314e67c363cda1256
Jake <jcmartin987@gmail.com>
2020-10-27 01:52:56 -0600

    lab9

--- lab_9/Lab_Website_3/server.js
+++ lab_9/Lab_Website_3/server.js
@@ -183,24 +183,24 @@ app.post('/home/pick_color', function(req, res) {
 
 // team stats page
 app.get('/team_stats', function(req, res) {
-  var q_games = 'SELECT * FROM football_games;'//'SELECT game_date, visitor_name, home_score, visitor_score, CASE WHEN visitor_score > home_score THEN visitor_name ELSE \'CU Boulder\' END AS winner FROM football_games;'
+  var q_games = 'SELECT game_date, visitor_name, home_score, visitor_score, CASE WHEN visitor_score > home_score THEN visitor_name ELSE \'CU Boulder\' END AS winner FROM football_games;';
   db.task('get-everything', task => {
       return task.batch([
           task.any(q_games),
       ]);
   })
   .then(info => {
-  	res.render('/team_stats',{
+  	res.render('/team_stats', {
   			my_title: "Team Stats",
   			games: info[0]
   		})
   })
   .catch(err => {
       // display error message in case an error
-          console.log('error', err);
-          res.render('/team_stats',{
-  			my_title: "Team Stats",
-  			games: ''
+      console.log('error', err);
+      response.render('/team_stats', {
+  		    my_title: "Team Stats",
+  			  games: ''
   		})
   });
 });
